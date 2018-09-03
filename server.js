2d6184ec5d06f6b6862379f7dd2ca0bc6e629735
elf Pavlik <elf-pavlik@hackers4peace.net>
2018-09-03 17:35:19 -0500

    updated to match current vientos-deploy

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const execOptions = {
 
 gh.listen()
 
-// TODO: replace SCRIPTS_PATH with a path to each script
 gh.on('push:vientos-app:' + GIT_REF, (data) => {
   console.log('APP')
-  execFile(`${SCRIPTS_PATH}/update-app.sh`, execOptions, (err, stdout, stderr) => {
+  execFile(`${SCRIPTS_PATH}/update-app`, execOptions, (err, stdout, stderr) => {
     if (err) console.log(err)
     if (stderr) console.log(stderr)
     console.log(stdout)
@@ -29,7 +28,7 @@ gh.on('push:vientos-app:' + GIT_REF, (data) => {
 
 gh.on('push:vientos-service:' + GIT_REF, (data) => {
   console.log('SERVICE')
-  execFile(`${SCRIPTS_PATH}/update-service.sh`, execOptions, (err, stdout, stderr) => {
+  execFile(`${SCRIPTS_PATH}/update-service`, execOptions, (err, stdout, stderr) => {
     if (err) console.log(err)
     if (stderr) console.log(stderr)
     console.log(stdout)
@@ -38,7 +37,25 @@ gh.on('push:vientos-service:' + GIT_REF, (data) => {
 
 gh.on('push:vientos-idp:' + GIT_REF, (data) => {
   console.log('IDP')
-  execFile(`${SCRIPTS_PATH}/update-idp.sh`, execOptions, (err, stdout, stderr) => {
+  execFile(`${SCRIPTS_PATH}/update-idp`, execOptions, (err, stdout, stderr) => {
+    if (err) console.log(err)
+    if (stderr) console.log(stderr)
+    console.log(stdout)
+  })
+})
+
+gh.on('push:vientos-website:' + GIT_REF, (data) => {
+  console.log('WEBSITE')
+  execFile(`${SCRIPTS_PATH}/update-website`, execOptions, (err, stdout, stderr) => {
+    if (err) console.log(err)
+    if (stderr) console.log(stderr)
+    console.log(stdout)
+  })
+})
+
+gh.on('push:vientos-webhooks:' + GIT_REF, (data) => {
+  console.log('WEBHOOKS')
+  execFile(`${SCRIPTS_PATH}/update-webhooks`, execOptions, (err, stdout, stderr) => {
     if (err) console.log(err)
     if (stderr) console.log(stderr)
     console.log(stdout)
